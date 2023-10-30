8be78edcbe5bd3ed0d154cc4fa05c7ae8dd2771d
Mujeeb Sulayman <sulaymanmujeeb6@Gmail.com>
2023-10-30 10:42:57 +0100

    chaCN/UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
+import AuthLayout from './_auth/AuthLayout';
 import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+import RootLayout from './_root/RootLayout';
 import Home from './_root/pages/Home';
 import './globals.css';
 import { Routes, Route } from 'react-router-dom';
@@ -8,16 +11,24 @@ const App = () => {
 		<main className='flex h-screen'>
 			<Routes>
 				{/* {PUBLIC ROUTES} */}
-				<Route
-					path='/sign-in'
-					element={<SigninForm />}
-				/>
-
+				<Route element={<AuthLayout />}>
+					<Route
+						path='/sign-in'
+						element={<SigninForm />}
+					/>
+					<Route
+						path='/sign-up'
+						element={<SignupForm />}
+					/>
+				</Route>
 				{/* PRIVATE ROUTES */}
-				<Route
-					index
-					element={<Home />}
-				/>
+
+				<Route element={<RootLayout/>}>
+					<Route
+						index
+						element={<Home />}
+					/>
+				</Route>
 			</Routes>
 		</main>
 	);
