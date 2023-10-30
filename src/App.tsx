bf95db645dfb3a02a43160754e844e46cd96b138
Mujeeb Sulayman <sulaymanmujeeb6@Gmail.com>
2023-10-30 10:14:50 +0100

    pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import './globals.css'
+import SigninForm from './_auth/forms/SigninForm';
+import Home from './_root/pages/Home';
+import './globals.css';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+	return (
+		<main className='flex h-screen'>
+			<Routes>
+				{/* {PUBLIC ROUTES} */}
+				<Route
+					path='/sign-in'
+					element={<SigninForm />}
+				/>
 
-export default App
+				{/* PRIVATE ROUTES */}
+				<Route
+					index
+					element={<Home />}
+				/>
+			</Routes>
+		</main>
+	);
+};
+
+export default App;
