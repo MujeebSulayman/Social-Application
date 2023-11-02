94eb0e578ee9a65cd963a560feb70e1dd60e0a8e
Mujeeb Sulayman <sulaymanmujeeb6@Gmail.com>
2023-11-02 18:17:27 +0100

    Done

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,7 +1,8 @@
 import {
-
+	useQuery,
 	useMutation,
-
+	useQueryClient,
+	useInfiniteQuery,
 } from '@tanstack/react-query';
 
 import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api';
