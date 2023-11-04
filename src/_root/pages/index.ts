7e3ecbaf886f0ac7f3602c4285d16802bc312c03
Mujeeb Sulayman <sulaymanmujeeb6@Gmail.com>
2023-11-04 21:00:42 +0100

    index

--- src/_root/pages/index.ts
+++ src/_root/pages/index.ts
@@ -5,6 +5,5 @@ export { default as PostDetails } from './PostDetails';
 export { default as Profile } from './Profile';
 export { default as UpdateProfile } from './UpdateProfile';
 export { default as EditPost } from './EditPost';
-export { default as Explore } from './Explore';
 export { default as CreatePost } from './CreatePost';
 export { default as LikedPosts } from './LikedPosts';
