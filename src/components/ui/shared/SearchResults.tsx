d51184643f8bf8dab64cc874dfe52904b60949a1
Mujeeb Sulayman <sulaymanmujeeb6@Gmail.com>
2023-11-04 19:40:41 +0100

    Explore

--- src/components/ui/shared/SearchResults.tsx
+++ src/components/ui/shared/SearchResults.tsx
@@ -12,8 +12,8 @@ const SearchResults = ({
 	searchedPosts,
 }: SearchResultsProps) => {
 	if (isSearchFetching) return <Loader />;
-	if (searchedPosts && searchedPosts.documents.length > 0) {
-		return <GridPostList posts={searchedPosts.documents} />;
+	if (searchedPosts && searchedPosts.length > 0) {
+		return <GridPostList posts={searchedPosts} />;
 	}
 
 	return (
